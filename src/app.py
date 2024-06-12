f66f65266c0e6707489f2e814182381a764def26
PrajjwalLyzr <prajjwal@lyzr.ai>
2024-06-13 02:39:22 +0530

    changes for zeet deployment

--- src/app.py
+++ src/app.py
@@ -13,3 +13,9 @@ app.register_blueprint(CompetitorAnalysis, url_prefix='')
 def index():
     return render_template('index.html')
 
+
+
+
+if __name__ == "__main__":
+    app.run(debug=False, host="0.0.0.0")
+
